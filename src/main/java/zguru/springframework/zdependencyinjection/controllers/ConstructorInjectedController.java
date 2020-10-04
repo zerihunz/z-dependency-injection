2f89b19fdfc2665a80160d7cf2eb1032f9fae076
ztegegn <zerihun.tegegn@gmail.com>
2020-10-03 21:00:56 -0400

    remove @Autowired Annotation as spring won't need it, it's automatically identified if the Dependency is injected via a Constructor

--- src/main/java/zguru/springframework/zdependencyinjection/controllers/ConstructorInjectedController.java
+++ src/main/java/zguru/springframework/zdependencyinjection/controllers/ConstructorInjectedController.java
@@ -1,11 +1,15 @@
 package zguru.springframework.zdependencyinjection.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import zguru.springframework.zdependencyinjection.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
     //Constructor injected is the most preferred way of DI.
     private final GreetingService greetingService;
 
+    //The Autowired annotation is not required when using a Constructor for DI, Spring will automatically detect it.
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
