d5b7da39552537e87a042c37bd9a0ccf83c64bd2
ztegegn <zerihun.tegegn@gmail.com>
2020-10-03 20:53:19 -0400

    Properly Annotate to allow inject dependency, Autowire and Controller annotations to the most preferred way of DI, Constructor Injected.

--- src/main/java/zguru/springframework/zdependencyinjection/ZDependencyInjectionApplication.java
+++ src/main/java/zguru/springframework/zdependencyinjection/ZDependencyInjectionApplication.java
@@ -3,6 +3,7 @@ package zguru.springframework.zdependencyinjection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import zguru.springframework.zdependencyinjection.controllers.ConstructorInjectedController;
 import zguru.springframework.zdependencyinjection.controllers.MyController;
 import zguru.springframework.zdependencyinjection.controllers.PropertyInjectedController;
 import zguru.springframework.zdependencyinjection.controllers.SetterInjectedController;
@@ -30,6 +31,11 @@ public class ZDependencyInjectionApplication {
 
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("---------------Constructor");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
